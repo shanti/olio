99fe31580849e3609dd424265c28cfcdcb02da9e
wsobel <wsobel@13f79535-47bb-0310-9956-ffa450edef68>
2009-02-24 01:16:21 +0000

    Fixed xpath for UIDriver
    git-svn-id: http://svn.apache.org/repos/asf/incubator/olio@747246 13f79535-47bb-0310-9956-ffa450edef68

--- workload/rails/trunk/src/org/apache/olio/workload/driver/UIDriver.java
+++ workload/rails/trunk/src/org/apache/olio/workload/driver/UIDriver.java
@@ -319,11 +319,11 @@ public class UIDriver {
         driverMetrics = new UIDriverMetrics();
         ctx.attachMetrics(driverMetrics);
         String hostPorts = ctx.getXPathValue(
-                                "/web20/webServer/fa:hostConfig/fa:hostPorts");
+                                "/olio/webServer/fa:hostConfig/fa:hostPorts");
         List<NameValuePair<Integer>> hostPortList =
                                             Utilities.parseHostPorts(hostPorts);
         int loadedScale = Integer.parseInt(
-                                    ctx.getXPathValue("/web20/dbServer/scale"));
+                                    ctx.getXPathValue("/olio/dbServer/scale"));
         loadedUsers = 4 * loadedScale;
         if (scale > loadedScale)
             throw new FatalException("Data loaded only for " + loadedScale +
