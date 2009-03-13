c443966090848f0cff5ea2bd352e8f9e4c7bfbbe
akara <akara@13f79535-47bb-0310-9956-ffa450edef68>
2009-03-13 05:16:30 +0000

    Removed import org.apache.jasper.tagplugins.jstl.core.Out thus allowing it to build on just about any servlet library.
    git-svn-id: http://svn.apache.org/repos/asf/incubator/olio@753119 13f79535-47bb-0310-9956-ffa450edef68

--- geocoder/src/org/apache/olio/geocoder/GeocoderServlet.java
+++ geocoder/src/org/apache/olio/geocoder/GeocoderServlet.java
@@ -31,7 +31,6 @@ import javax.servlet.http.*;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import org.apache.jasper.tagplugins.jstl.core.Out;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.DOMException;
