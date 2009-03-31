105f0b3b8c7ffab82906cc9a9b0347c0324667bc
shanti <shanti@13f79535-47bb-0310-9956-ffa450edef68>
2009-03-31 21:26:16 +0000

    Fix for OLIO-82. Added license hdrs.
    git-svn-id: http://svn.apache.org/repos/asf/incubator/olio@760652 13f79535-47bb-0310-9956-ffa450edef68

--- webapp/rails/trunk/app/controllers/users_controller.rb
+++ webapp/rails/trunk/app/controllers/users_controller.rb
@@ -1,3 +1,20 @@
+#
+#  Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 class UsersController < ApplicationController
   
   before_filter :authorize, :except => [ :new, :create, :login, :check_name, :show ]
@@ -32,8 +49,11 @@ class UsersController < ApplicationController
   # GET /users/1.xml
   def show
     @user = User.find(params[:id])
-	# Shanti: removing requirement for being logged in
-    # @you = User.find(session[:user_id])
+	if session[:user_id].nil?
+	  @you = nil
+	else
+	  @you = User.find(session[:user_id])
+	end 
     @address = Address.find(@user.address_id)
     @image = @user.image
     @posted = @user.posted_events[0..2]
